e0a80e2de8eb4abe707ee11beb1ba340b66fe0ee
srujana <srujanakaranam3020@gmail.com>
2025-08-21 19:34:09 +0530

    oject

---
+++ object.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "Srujana",
+    age: 21,
+    education: "Btech-CSE-CyberSecurity"
+};
+
+console.log("Student Object:", student);
+console.log(`Name: ${student.name}, Age: ${student.age}, Education: ${student.education}`);
