b4fed22af838d2f997dca8a07af1b491d4b712c1
srujana <srujanakaranam3020@gmail.com>
2025-08-21 19:12:15 +0530

    product

---
+++ product.js
@@ -0,0 +1,19 @@
+//normal function
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(3,4));
+
+
+//function expression
+
+const multipler = function(a,b){
+    return a*b;
+}
+console.log(multipler(5,6));
+
+//arrow function
+
+const multiplied=(a,b)=>a*b;
+console.log(multiplied(2,6));
