a2022938c9ce849fb8dde7c0cb33d62b3bfa2301
srujana <srujanakaranam3020@gmail.com>
2025-08-21 19:40:16 +0530

    array

---
+++ array.js
@@ -0,0 +1,5 @@
+let fruits = ["pineapple","strawberry","oranges"];
+console.log("fruits are: "+fruits);
+console.log("first fruit: "+fruits[0]);
+console.log("second fruit: "+fruits[1]);
+console.log("third fruit: "+fruits[2]);
