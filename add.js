fbd2a85e84ced6da7f6162e91116b1d61f86c09e
srujana <srujanakaranam3020@gmail.com>
2025-08-21 19:11:24 +0530

    add

---
+++ add.js
@@ -0,0 +1,19 @@
+//normal function
+
+function addNumbers(a,b){
+    return a+b;
+}
+console.log(addNumbers(3,4));
+
+
+//function expression
+
+const addnumbers = function(a,b){
+    return a+b;
+}
+console.log(addnumbers(5,6));
+
+//arrow function
+
+const addNumber=(a,b)=>a+b;
+console.log(addNumbers(2,6));
